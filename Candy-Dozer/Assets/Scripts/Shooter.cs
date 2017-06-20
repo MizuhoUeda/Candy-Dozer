d43f2c75c321177ad676e7808fa205672fd3c0cb
mizuho <mizu.2340@gmail.com>
2017-06-20 23:45:18 +0900

    shooter test

---
+++ Candy-Dozer/Assets/Scripts/Shooter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+	public GameObject candyPrefab;
+	public float shotSpeed;
+	public float shotTorque;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetButtonDown("Fire1")) Shot();
+	}
+
+	public void Shot()
+	{
+		GameObject candy = (GameObject)Instantiate(
+			candyPrefab,
+			transform.position,
+			Quaternion.identity
+			);
+
+		Rigidbody candyRigidBody = candy.GetComponent<Rigidbody>();
+		candyRigidBody.AddForce(transform.forward * shotSpeed);
+		candyRigidBody.AddTorque(new Vector3(0,shotTorque,0));
+	}
+}
